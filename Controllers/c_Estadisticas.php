0e7c0c8d8b15864c7e42beca1cfda7808e5ab3f2
Raul Villar <rvramos@hotmail.es>
2016-11-17 22:12:18 +0100

    Add files via upload

--- Controllers/c_Estadisticas.php
+++ Controllers/c_Estadisticas.php
@@ -2,7 +2,12 @@
 <?php
 require_once("../Model/Estadistica.php");
 require_once("../DB/connectDB.php");
+require_once("../Controllers/c_Usuario.php");
 
+session_start();
+
+$userController = new UsuarioController();
+$user = $userController->getUserByEmail($_SESSION['userID']);
 //TODO TODA LA CLASE
 class EstatController{
   function __construct(){
@@ -15,15 +20,19 @@ $e = new Estadistica();
 
 foreach ($_POST['arrayID'] as $it) {
   $done = 0;
-  if(isset($_POST['arrayStats'][$i])){
+
+  if(isset($_POST['arrayStats'][$i])&&isset($_POST['arrayID'][$i])){
     $done=1;
   }
 
-  if($done == 1){
-    $e->createEstat($_POST['arrayID'][$i],$_POST['idTabla'],$_POST['idUser']); //chekar ultimos 2
+  if($done == 1){ //$_POST['arrayID'][$i] da problemas Why?????
+  $e->createEstat($_POST['arrayID'][$i],$_POST['idTabla'],$user['idUsuario']); //chekar ultimos 2
+  echo('<br> - Paso'.$i." Da : <br> ".$_POST['arrayID'][$i]." -- ".$_POST['idTabla']." -- ".$user['idUsuario']);
   }
   $i++;
+
 }
-header('Location: ' . $_SERVER['HTTP_REFERER']);
+
+//header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 ?>
