a4aa58e251359dda56a38b2df3b3b5e6730bf2c3
Raul Villar <rvramos@hotmail.es>
2017-01-03 15:30:33 +0100

    update tocho

--- Views/LogIn.php
+++ Views/LogIn.php
@@ -4,9 +4,17 @@
 require_once('NavBar.php');
 require_once('../DB/connectDB.php');
 ?>
-<form method="post" action="../Controllers/c_Usuario.php?op=3">
-email:  <input type="text" name="email" placeholder="ejemplo@gmail.com"/></br>
-pass:   <input type="password" name="pass" placeholder="************"/></br>
-<input type="hidden" name="idUsuario" value="placeholder"/><!--necesario para reutilizar el controller de usuario-->
-<input type="submit" value="login"/>
-</form>
+	<!--estilos para el botn de login -->
+<link href="../bootstrap/custom.css" rel="stylesheet">
+<div class="container">
+
+	<form method="post" action="../Controllers/c_Usuario.php?op=3" class="form-signin">
+	<h2 class="form-signin-heading">Porfavor logeate</h2>
+	email:  <input type="text" name="email" placeholder="ejemplo@gmail.com"/></br>
+	pass:   <input type="password" name="pass" placeholder="************"/></br>
+	<input type="hidden" name="idUsuario" value="placeholder"/><!--necesario para reutilizar el controller de usuario-->
+	<br>
+	<input type="submit" value="login" class="btn btn-lg btn-primary btn-block"/>
+
+	</form>
+</div>
