0158087638414efebbf8f8a1c5794bf5d47723d6
cyanide4all <marvazto@gmail.com>
2017-01-14 17:31:00 +0100

    Js casi full

--- Views/CrearUsuario.php
+++ Views/CrearUsuario.php
@@ -14,27 +14,27 @@
 <body>
   <div class="tabla">
     <div class="row">
-      <form method="post" action="../Controllers/c_Usuario.php?op=2">
-        <div class="row"><span class="col-md-2">Tipo de Usuario:</span> <select name="tipoDeUsuario">
+      <form method="post" action="../Controllers/c_Usuario.php?op=2" onsubmit="return validarNuevoUsuario()">
+        <div class="row"><span class="col-md-2">Tipo de Usuario:</span> <select id='tipoU' name="tipoDeUsuario">
                         <option value="">--Selecionar--</option>
                         <option value="admin">Administrador</option>
                         <option value="entrenador">Entrenador</option>
                         <option value="deportista">Deportista</option>
                       </select></div>
-        <div class="row"><span class="col-md-2">Nombre:</span> <input type="text" name="nomUsuario" placeholder="Nombre"  /></div>
-        <div class="row"><span class="col-md-2">Direccion:</span> <input type="text" name="dircUsuario" placeholder="Direccion" /></div>
-        <div class="row"><span class="col-md-2">Telefono:</span> <input type="text" name="telfUsuario" placeholder="Telefono" /></div>
-      <div class="row"><span class="col-md-2">  Tipo de Tarjeta:</span> <select name="tipoTarjetaUsuario">
+        <div class="row"><span class="col-md-2">Nombre:</span> <input id='nombre' type="text" name="nomUsuario" placeholder="Nombre"  /></div>
+        <div class="row"><span class="col-md-2">Direccion:</span> <input id='direccion' type="text" name="dircUsuario" placeholder="Direccion" /></div>
+        <div class="row"><span class="col-md-2">Telefono:</span> <input id='telefono' type="text" name="telfUsuario" placeholder="Telefono" /></div>
+      <div class="row"><span class="col-md-2">  Tipo de Tarjeta:</span> <select id='tipoT' name="tipoTarjetaUsuario">
                         <option value="">--Selecionar--</option>
                         <option value="TDU">TDU</option>
                         <option value="PEF">PEF</option>
                       </select></div>
-      <div class="row"><span class="col-md-2">  Dni:</span> <input type="text" name="dniUsuario" placeholder="Dni" /></div>
-        <div class="row"><span class="col-md-2">Fecha de Nacimiento:</span> <input type="text" name="fechNacUsuario" placeholder="AAAA:MM:DD" /></div>
-      <div class="row"><span class="col-md-2">  Email:</span> <input type="text" name="emailUsuario" placeholder="ejemplo@dominio.com" /></div>
-      <div class="row"><span class="col-md-2">  Contraseña:</span> <input type="password" name="passUsuario" placeholder="Pass" /></div>
-      <div class="row"><span class="col-md-2">Repetir Contraseña:</span> <input type="password" name="passUsuario2" placeholder="Pass" /></div>
-          <input class="btn btn-success" type="submit" name="idUsuario">
+      <div class="row"><span class="col-md-2">  Dni:</span> <input id='dni' type="text" name="dniUsuario" placeholder="Dni" /></div>
+        <div class="row"><span class="col-md-2">Fecha de Nacimiento:</span> <input id='fechaNac' type="text" name="fechNacUsuario" placeholder="AAAA:MM:DD" /></div>
+      <div class="row"><span class="col-md-2">  Email:</span> <input id='email' type="text" name="emailUsuario" placeholder="ejemplo@dominio.com" /></div>
+      <div class="row"><span class="col-md-2">  Contraseña:</span> <input id='ps1' type="password" name="passUsuario" placeholder="Pass" /></div>
+      <div class="row"><span class="col-md-2">Repetir Contraseña:</span> <input id='ps2' type="password" name="passUsuario2" placeholder="Pass" /></div>
+          <input class="btn btn-success" type="submit" name="idUsuario" value="Crear Usuario">
       </form>
     </div>
     </div>
