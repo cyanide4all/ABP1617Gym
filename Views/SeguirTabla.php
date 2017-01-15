bb3a7867e056d57949eca62387ec715df687703f
Raul Villar <rvramos@hotmail.es>
2017-01-15 17:09:50 +0100

    css

--- Views/SeguirTabla.php
+++ Views/SeguirTabla.php
@@ -1,4 +1,14 @@
 <!--Martín 16/11/16-->
+<?php
+//ESTAR LOGEADO AL MENOS
+if(!isset($_SESSION))
+{
+    session_start();
+}
+if(!isset($_SESSION['userID'])){
+  header('Location: paginaPrincipal.php');
+}else{
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,18 +18,16 @@
     require_once('../Controllers/c_Tabla.php');
     require_once('../Controllers/c_Ejercicio.php');
     require_once("../DB/connectDB.php");
-
     $controlEjer = new EjercicioController();
     $controlTabla = new TablaController();
-
     $listaEjercicios = $controlTabla->getEjerciciosTabla($_GET['id']);
     ?>
   </head>
 
   <body>
-    <div id = 'tabla2elementos' >
-      <div class = 'tupla'>
-        <a clas = 'izquierda'>Seguimiento de ejercicios</a>
+    <div class = 'tabla panel-default' >
+        <div class = 'row panel-heading'>
+          <span class ="col-md-2">Seguimiento de ejercicios</span>
       </div>
 
       <form method= "post" action = "../Controllers/c_Estadisticas.php" class ='derecha' id="seguir">
@@ -27,19 +35,19 @@
       <?php
       $i = 0;
       foreach($listaEjercicios as $it){ ?>
-      <div class = 'tupla'>
+      <div class="row">
         <?php
           $actual = $controlEjer->getEjercicio($it['Ejercicio_idEjercicio']);
         ?>
-        <a clas = 'izquierda'><?php echo ($actual['nomEjercicio']); ?> </a>
-        <a clas = 'izquierda'><?php echo ($actual['descripcion']); ?> </a>
-        <a clas = 'izquierda'><?php echo ($it['nRepeticiones']); ?> </a>
-        <a clas = 'izquierda'><?php echo ($it['carga']); ?> </a>
+        <span class="col-md-1"><?php echo ($actual['nomEjercicio']); ?> </span>
+        <span class="col-md-1"><?php echo ($actual['descripcion']); ?> </span>
+        <span class="col-md-1"><?php echo ($it['nRepeticiones']); ?> </span>
+        <span class="col-md-1"><?php echo ($it['carga']); ?> </span>
 
         <input type="radio" name="arrayStats[<?php echo($i); ?>]" value="1"> Realizado
         <input type="radio" name="arrayStats[<?php echo($i); ?>]" value="0"> Pendiente
         <input type="text" name="comentario[<?php echo($i); ?>]" placeholder="Escribe aqui tus comentarios"/>
-        
+
         <input type="hidden" name="arrayID[<?php echo($i++); ?>]" value="<?php echo($it['Ejercicio_idEjercicio']); ?>"/>
         <input type="hidden" name ="idTabla" value="<?php echo($_GET['id']); ?>"/>
 
@@ -52,3 +60,6 @@
     </div>
   </body>
 </html>
+<?php
+}
+?>
